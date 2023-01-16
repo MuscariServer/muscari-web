f2a510f6bbee5b94e58a1a31b581bf5ca7020a32
morlef <49605393+eighty88@users.noreply.github.com>
2023-01-16 23:04:18 +0900

    Update MenuItem

--- src/components/menuitem.tsx
+++ src/components/menuitem.tsx
@@ -2,9 +2,9 @@ import './menu.css';
 
 import { Link as Scroll } from 'react-scroll';
 
-export default function MenuItem(to: string, text: string, active: boolean) {
+export const MenuItem = (to: string, text: string, active: boolean) => {
     return (
-        <div className={active ? "ActiveMenuItem" : "MenuItem"}>
+        <div className={active ? "MenuItem -active" : "MenuItem"}>
             <Scroll to={to} smooth={true} duration={500}>
                 {text}
             </Scroll>
